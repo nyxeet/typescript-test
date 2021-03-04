7dc4ccd4aa33c4025587f527a958a9ddf17405c5
nyxeet <nyxeet@ukr.net>
2021-03-04 13:12:34 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
 import React from 'react';
 
-let a: number | null = 10;
-
-a = null;
-
-let names: Array<string> = ['1', '2', '3']
-type adressType = {
-  city: string | null
-  index: number | null
-}
-type objTypes = {
-  name: string | null
-  age: number | null
-  adress: adressType | null
-
-}
-const obj = {
-  name: "dima",
-  age: 12,
-  adress: {
-    city: 'kiyv',
-    index: 12345,
-  }
-}
-
-
 function App() {
-  return (
-    <div className="App"></div>
-  );
+  let a: number | null = 10;
+
+  a = null;
+
+  let names: Array<string> = ['1', '2', '3'];
+  type adressType = {
+    city: string | null;
+    index: number | null;
+  };
+  type objTypes = {
+    name: string | null;
+    age: number | null;
+    adress: adressType | null;
+  };
+  const obj = {
+    name: 'dima',
+    age: 12,
+    adress: {
+      city: 'kiyv',
+      index: 12345,
+    },
+  };
+  return <div className="App"></div>;
 }
 
 export default App;
